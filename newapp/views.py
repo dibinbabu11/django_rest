40a23b9c2551f0c6343148aed8a8c4fbb86f95a5
ADMINISTRTOR <dibinbabu11>
2025-05-23 16:25:22 +0530

    first commit

--- newapp/views.py
+++ newapp/views.py
@@ -7,7 +7,7 @@ from rest_framework.decorators import api_view
 # Create your views here.
 
 
-@api_view(['GET','POST'])
+@api_view(['GET','POST','PUT', 'DELETE'])
 def index(request):
     if request.method=='GET':
         student=Student.objects.all()
@@ -20,4 +20,27 @@ def index(request):
             serializer.save()
             return JsonResponse(serializer.data,status=status.HTTP_201_CREATED)
         return JsonResponse(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'PUT':
+        try:
+            student = Student.objects.get(pk=request.data.get('id'))
+            serializer = studentSerializers(student, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return JsonResponse(serializer.data)
+        except Student.DoesNotExist:
+            return JsonResponse({'error': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
+
+       
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        try:
+            student = Student.objects.get(pk=request.data.get('id'))
+        except Student.DoesNotExist:
+            return JsonResponse({'error': 'Student not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        student.delete()
+        return JsonResponse({'message': 'Student deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+
+
     return JsonResponse()
